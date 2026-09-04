dced766ce8f9589199858f025aacede6cc72cc97
belowaqa <below.nulll@gmail.com>
2026-09-04 19:08:19 +0100

    add fixture for registration

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,52 +1,23 @@
-import os
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Page
 
 
-@pytest.mark.courses
-@pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as pw:
-        browser = pw.chromium.launch(headless=True)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user@gmail.com')
+def test_empty_courses_list(chromium_page_with_state : Page):
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
+    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
+    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
+    dir_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(dir_icon).to_be_visible()
 
-        context.storage_state(path='cookie/browser-state.json')
+    result_title_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(result_title_text).to_be_visible()
+    expect(result_title_text).to_have_text('There is no results')
 
-    with sync_playwright() as pw:
-        browser = pw.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='cookie/browser-state.json')
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_title).to_be_visible()
-        expect(courses_title).to_have_text('Courses')
-
-        dir_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(dir_icon).to_be_visible()
-
-        result_title_text = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(result_title_text).to_be_visible()
-        expect(result_title_text).to_have_text('There is no results')
-
-        description_text = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(description_text).to_be_visible()
-        expect(description_text).to_have_text('Results from the load test pipeline will be displayed here')
-
-        if os.path.exists('cookie/browser-state.json'):
-            os.remove('cookie/browser-state.json')
+    description_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(description_text).to_be_visible()
+    expect(description_text).to_have_text('Results from the load test pipeline will be displayed here')
